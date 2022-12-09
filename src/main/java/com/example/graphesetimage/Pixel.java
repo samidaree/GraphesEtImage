a602fd54223ba33cbd4b279c3d4d6b5b8375c4e0
Sami <117381937+samidaree@users.noreply.github.com>
2022-12-09 20:45:30 +0100

    UI Final Version

---
+++ src/main/java/com/example/graphesetimage/Pixel.java
@@ -0,0 +1,18 @@
+package com.example.graphesetimage;
+
+public class Pixel {
+    private int x, y;
+
+    public Pixel (int x, int y){
+        this.x = x;
+        this.y = y ;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y ;
+    }
+}
